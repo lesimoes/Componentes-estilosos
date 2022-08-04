b39cc54c6da289a7d11a1b42d610f5fa2799c346
lesimoes <lesimoes0@gmail.com>
2022-08-04 15:00:16 -0300

    basio, do basico...

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,80 @@
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
 export default function Card({ id, name, image, type }) {
+  
   return (
-    <div className={`Card ${type}`}>
+    <Wrapper type={type}>
       <span>{name}</span>
       <img src={image} alt={name} />
       <Link to={`/detail/${id}`}>
         <button>Detalhes</button>
       </Link>
-    </div>
+    </Wrapper>
   );
 }
+
+
+const Wrapper = styled.div`
+  background-color: crimson;
+  border-radius: 4px;
+  width: 200px;
+  height: 300px;
+  margin: 10px;
+
+  &:hover {
+    filter: brightness(120%);
+  }
+
+  img {
+    width: 80%;
+    height: 80%;
+  }
+
+  span {
+    color: white;
+    font-size: 18px;
+    font-weight: bold;
+  }
+
+  button {
+    border: none;
+    border-radius: 2px;
+    width: 100px;
+    height: 30px;
+    font-size: 14px;
+    color: white;
+    background-color: crimson;
+    cursor: pointer;
+  }
+
+  ${props => {
+    if (props.type === 'grass') {
+      return `
+        background-color: lightgreen;
+      `
+    }
+    if (props.type === 'fire') {
+      return `
+        background-color: lightcoral;
+      `
+    }
+
+    if (props.type === 'water') {
+      return `
+        background-color: lightskyblue;
+      `
+    }
+    if (props.type === 'bug') {
+      return `
+        background-color: lightgray;
+        span {
+         color: black;
+        }
+      `
+    }
+  }}
+
+`
+
+
